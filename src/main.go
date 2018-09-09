547960585ea85062e9babdde0175dd41e011f134
Bradford Bazemore <bankersalgo@gmail.com>
2018-09-08 21:18:30 -0400

    added gcp build files for pr testings and for auto deploy to staging. I also moved the test html and set to static file deliver. Middleware is now its own package.

--- src/main.go
+++ src/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"google.golang.org/appengine"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"server"
 )
 
-func init() {
+func main() {
 	router := gin.New()
 	router.Use(gin.ErrorLogger())
 	router.Use(gin.Recovery())
 	server.ApiEntry(router)
 	http.Handle("/", router)
+
+	appengine.Main()
 }
